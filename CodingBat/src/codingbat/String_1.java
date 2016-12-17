c0c64c92fabc25d266dba1163432067ccffd8ee2
Inquis1t0r <pi0tr3k9292@gmail.com>
2016-12-18 00:20:16 +0100

    Create String_1.java

---
+++ CodingBat/src/codingbat/String_1.java
@@ -0,0 +1,13 @@
+package codingbat;
+
+/**
+ *
+ * @author inquis1t0r
+ */
+public class String_1 {
+    
+  public String helloName(String name) {
+        return ("Hello " + name + "!");
+    }
+
+}
