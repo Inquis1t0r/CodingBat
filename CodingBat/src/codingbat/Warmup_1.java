e09bcbd602b95222ba3cc7f29fcfae332b52eca2
inquis1t0r <pi0tr3k9292@gmail.com>
2017-01-31 19:35:49 +0100

    makes10 solution added

--- CodingBat/src/codingbat/Warmup_1.java
+++ CodingBat/src/codingbat/Warmup_1.java
@@ -92,6 +92,14 @@ public class Warmup_1 {
             else
             return false;
             }
+        
+            public boolean makes10(int a, int b) {
+                if(a == 10 || b == 10 ||a + b == 10)
+                return true;
+                else
+                return false;
+            }
+
  
    
 }
