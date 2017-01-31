652ca238de85ac85b9c97ad195bb0cab1754b029
inquis1t0r <pi0tr3k9292@gmail.com>
2017-01-31 19:29:59 +0100

    posNeg solution added, and folder made in IDE test

--- CodingBat/src/codingbat/Warmup_1.java
+++ CodingBat/src/codingbat/Warmup_1.java
@@ -80,6 +80,19 @@ public class Warmup_1 {
             }
 
         }
+        
+        public boolean posNeg(int a, int b, boolean negative) {
+
+            if(a < 0 && b > 0 && negative == false)
+            return true;
+            else if (a > 0 && b < 0 && negative == false)
+            return true;
+            else if (a < 0 && b < 0 && negative == true)
+            return true;
+            else
+            return false;
+            }
+ 
    
 }
 
