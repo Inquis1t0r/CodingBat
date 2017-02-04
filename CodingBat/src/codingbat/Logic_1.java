7c04f0b048efb9d24893a7e78dd740d06997c237
inquis1t0r <pi0tr3k9292@gmail.com>
2017-02-04 12:57:27 +0100

    redTicket solution added

--- CodingBat/src/codingbat/Logic_1.java
+++ CodingBat/src/codingbat/Logic_1.java
@@ -66,6 +66,21 @@ public boolean cigarParty(int cigars, boolean isWeekend) {
     return false;
 }
 
+public int redTicket(int a, int b, int c) {
+  int result;
+  
+  if(a == b && a == c && b == c && a == 2)
+  result = 10;
+  else if(a == b && a == c && b == c)
+  result = 5;
+  else if(a != b && a != c)
+  result = 1;
+  else
+  result = 0;
+  
+  return result;  
+}
+
 
 
 
