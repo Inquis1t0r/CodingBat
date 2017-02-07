808627ce92a29cb1e5dd1c1fb6d4a480b3cb449a
inquis1t0r <pi0tr3k9292@gmail.com>
2017-02-07 16:45:40 +0100

    love6 solution added, and some readability fixes

--- CodingBat/src/codingbat/Logic_1.java
+++ CodingBat/src/codingbat/Logic_1.java
@@ -14,83 +14,88 @@ public class Logic_1 {
     
 
 
-public boolean in1To10(int n, boolean outsideMode) {
-  if (outsideMode == false){
-    if (n >= 1 && n <= 10) {
-      return true;
-    } 
-    else 
-      {
-        return false;
-      }
-  } 
-  else
-  {
-      if (n <= 1 || n >= 10) {
-        return true;
+    public boolean in1To10(int n, boolean outsideMode) {
+      if (outsideMode == false){
+        if (n >= 1 && n <= 10) {
+          return true;
+        } 
+        else 
+          {
+            return false;
+          }
       } 
-      else 
-        {
-          return false;
+      else
+      {
+          if (n <= 1 || n >= 10) {
+            return true;
+          } 
+          else 
+            {
+              return false;
+            }
         }
     }
-}
-public String alarmClock(int day, boolean vacation) {
-  if (vacation) 
-  {
-    if(day > 0 && day != 6)
-      return "10:00";
-    else
-      return "off";
-  } 
-  else if (day > 0 && day != 6)
-      return "7:00";
-    else
-      return "10:00";
-}
 
-public boolean specialEleven(int n) {
+    public String alarmClock(int day, boolean vacation) {
+      if (vacation) 
+      {
+        if(day > 0 && day != 6)
+          return "10:00";
+        else
+          return "off";
+      } 
+      else if (day > 0 && day != 6)
+          return "7:00";
+        else
+          return "10:00";
+    }
 
-  if(n%11 == 0 || n%11 == 1)
-  return true;
-  else
-  return false;
-}
+    public boolean specialEleven(int n) {
 
-public boolean cigarParty(int cigars, boolean isWeekend) {
-    if (isWeekend == true && cigars >= 40)
-    return true;
-  if (!isWeekend && cigars >=40 && cigars <= 60)
-    return true;
-  else
-    return false;
-}
+      if(n%11 == 0 || n%11 == 1)
+      return true;
+      else
+      return false;
+    }
 
-public int redTicket(int a, int b, int c) {
-  int result;
-  
-  if(a == b && a == c && b == c && a == 2)
-  result = 10;
-  else if(a == b && a == c && b == c)
-  result = 5;
-  else if(a != b && a != c)
-  result = 1;
-  else
-  result = 0;
-  
-  return result;  
-}
+    public boolean cigarParty(int cigars, boolean isWeekend) {
+        if (isWeekend == true && cigars >= 40)
+        return true;
+      if (!isWeekend && cigars >=40 && cigars <= 60)
+        return true;
+      else
+        return false;
+    }
 
-public boolean twoAsOne(int a, int b, int c) {
-  if (a + b == c || a + c == b || b+c == a)
-  return true;
-  else 
-  return false;
-}
+    public int redTicket(int a, int b, int c) {
+      int result;
 
+      if(a == b && a == c && b == c && a == 2)
+      result = 10;
+      else if(a == b && a == c && b == c)
+      result = 5;
+      else if(a != b && a != c)
+      result = 1;
+      else
+      result = 0;
 
+      return result;  
+    }
 
+    public boolean twoAsOne(int a, int b, int c) {
+      if (a + b == c || a + c == b || b+c == a)
+      return true;
+      else 
+      return false;
+    }
 
+    public boolean love6(int a, int b) {
 
+      if(a == 6 || b == 6 || a + b == 6 || Math.abs(a - b) == 6 || Math.abs(b - a) == 6)
+      return true;
+      else
+      return false;
+
+    }
 
 }
