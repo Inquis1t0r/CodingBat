10eba027ef71e27aba82d3edb3e7741e861e9665
inquis1t0r <pi0tr3k9292@gmail.com>
2017-02-12 14:58:40 +0100

    caughtSpeeding solutions added

--- CodingBat/src/codingbat/Logic_1.java
+++ CodingBat/src/codingbat/Logic_1.java
@@ -126,6 +126,14 @@ public class Logic_1 {
         return false;
     }
     
+    public int caughtSpeeding(int speed, boolean isBirthday) {
+        if(speed <= 60 || speed <= 65 && isBirthday == true)
+          return 0;
+        else if(speed >= 61 && speed <= 80 || speed >= 66 && speed <= 85 && isBirthday == true)
+          return 1;
+        else
+         return 2;
+    }
 
 
 }
