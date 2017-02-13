f013aa91d393074173bb5c7e4cecdf2bf90a544e
inquis1t0r <pi0tr3k9292@gmail.com>
2017-02-13 15:21:47 +0100

    teaParty solution added

--- CodingBat/src/codingbat/Logic_1.java
+++ CodingBat/src/codingbat/Logic_1.java
@@ -141,5 +141,16 @@ public class Logic_1 {
         else
         return false;
     }
+    
+    public int teaParty(int tea, int candy) {
+        if(tea < 5 || candy < 5)
+        return 0;
+        else if (tea >= 5  && tea < 2*candy && candy < 2*tea|| candy >= 5 && candy < 2*tea && tea < 2*candy)   //TODO: make this @#%$ shorter
+        return 1;
+        else if (tea >= 2*candy || candy >= 2*tea)
+        return 2;
+        else
+        return 3;
+    }
 
 }
