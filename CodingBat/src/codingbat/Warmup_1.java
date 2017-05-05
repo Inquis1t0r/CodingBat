68cfd37ce9041529c470c62684d5ac4e714b19cc
inquis1t0r <pi0tr3k9292@gmail.com>
2017-05-05 21:38:56 +0200

    solution for stringE  added

--- CodingBat/src/codingbat/Warmup_1.java
+++ CodingBat/src/codingbat/Warmup_1.java
@@ -111,8 +111,17 @@ public class Warmup_1 {
                 front =  str;
                 return String.join("", Collections.nCopies(3, front));
   
+            }        
+        public boolean stringE(String str) {
+                int count = 0;
+                for(int temp = 0; temp < str.length(); temp++)
+                {
+                if(str.charAt(temp) == 'e')
+                count++;
+                }
+                return (count >= 1 && count <= 3) ? true : false;
             }
-            //TODO rework ^
+        //TODO rework ^
 
  
    
