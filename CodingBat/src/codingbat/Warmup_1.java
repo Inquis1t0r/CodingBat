5fc4265f526c1bcaac0a64bb56aeaaad919f20ef
inquis1t0r <pi0tr3k9292@gmail.com>
2017-05-09 21:48:12 +0200

    icyHot solution added

--- CodingBat/src/codingbat/Warmup_1.java
+++ CodingBat/src/codingbat/Warmup_1.java
@@ -141,6 +141,11 @@ public class Warmup_1 {
         public boolean or35(int n) {
             return (n % 3 == 0) || (n % 5 == 0);
         }
+        
+        public boolean icyHot(int temp1, int temp2) {
+            return ((temp1 < 0 && temp2 > 100) || (temp1 > 100 && temp2 < 0));
+        }
+
 
         
         //TODO rework ^
