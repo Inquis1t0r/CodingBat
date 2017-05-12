21693924e6c2d079f52568e728d058fa012bef99
inquis1t0r <pi0tr3k9292@gmail.com>
2017-05-12 22:21:46 +0200

    makes10 refactor

--- CodingBat/src/codingbat/Warmup_1.java
+++ CodingBat/src/codingbat/Warmup_1.java
@@ -94,14 +94,11 @@ public class Warmup_1 {
             return true;
             else
             return false;
-            }
+         }
         
-            public boolean makes10(int a, int b) {
-                if(a == 10 || b == 10 ||a + b == 10)
-                return true;
-                else
-                return false;
-            }
+        public boolean makes10(int a, int b) {
+            return (a == 10 || b == 10 ||a + b == 10);
+        }
             
         public String front3(String str) {
                 String front;
