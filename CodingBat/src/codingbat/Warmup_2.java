66aa696f08bc90551e2c79879452707d87c587d3
Piotr Kazimierski <pi0tr3k9292@gmail.com>
2018-01-08 20:06:37 +0100

    stringTimes solution added

--- CodingBat/src/codingbat/Warmup_2.java
+++ CodingBat/src/codingbat/Warmup_2.java
@@ -6,4 +6,15 @@ package codingbat;
  */
 public class Warmup_2 {
     
+    public String stringTimes(String str, int n) {
+
+      String str2 = "";
+
+        for (int i=0; i<n; i++)
+        str2 += str;
+
+        return str2;
+    }
+
+    
 }
