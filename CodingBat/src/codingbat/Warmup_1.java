f7482dc52a47e1c06883f5b59b64440952f5ed74
Piotr Kazimierski <pi0tr3k9292@gmail.com>
2018-01-16 23:05:28 +0100

    close10 solution added

--- CodingBat/src/codingbat/Warmup_1.java
+++ CodingBat/src/codingbat/Warmup_1.java
@@ -212,6 +212,21 @@ public class Warmup_1 {
            String back = str.substring(str.length() - 1);
            return back + str + back;
         }
+    
+        public int close10(int a, int b) {
+            
+            int aAbs = Math.abs(a - 10);
+            int bAbs = Math.abs(b - 10);
+
+             if (aAbs < bAbs) {
+                return a;
+              }
+              else if (aAbs > bAbs)
+              return b;
+              else 
+              return 0;
+         }
+
 
 
       
