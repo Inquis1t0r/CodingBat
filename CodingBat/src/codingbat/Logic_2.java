342c50876cc72bae8d68f31553f9a9027fe053db
Piotr Kazimierski <pi0tr3k9292@gmail.com>
2018-01-19 21:45:48 +0100

    luckySum solution added

--- CodingBat/src/codingbat/Logic_2.java
+++ CodingBat/src/codingbat/Logic_2.java
@@ -11,4 +11,20 @@ package codingbat;
  */
 public class Logic_2 {
     
+        public int luckySum(int a, int b, int c) {
+              int answer = 0;
+
+              if (a != 13 && b != 13 && c != 13)
+              answer = a + b + c;
+              else if (a == 13)
+              answer =  0;
+              else if (a != 13 && b == 13 && (c != 13 || c == 13))
+              answer =  a;
+              else if (a != 13 && b != 13 && c == 13)
+              answer =  a + b;
+
+              return answer;
+        }
+
+    
 }
