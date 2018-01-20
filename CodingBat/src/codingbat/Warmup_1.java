f7b561223b42df983fb226b4cf5980822db8b90f
Piotr Kazimierski <pi0tr3k9292@gmail.com>
2018-01-20 22:45:10 +0100

    missingChar solution added

--- CodingBat/src/codingbat/Warmup_1.java
+++ CodingBat/src/codingbat/Warmup_1.java
@@ -239,7 +239,13 @@ public class Warmup_1 {
               return solution;
         }
 
+        public String missingChar(String str, int n) {
+            
+              String foo = str.substring(0, n);
+              String bar = str.substring(n+1, str.length());
 
+              return foo + bar;
+        }
       
 
  
