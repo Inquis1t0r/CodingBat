408cb4020c568575d7ef7f1286d86739e54f22ae
Piotr Kazimierski <pi0tr3k9292@gmail.com>
2018-01-26 19:55:49 +0100

    in3050 solution added

--- CodingBat/src/codingbat/Warmup_1.java
+++ CodingBat/src/codingbat/Warmup_1.java
@@ -246,6 +246,17 @@ public class Warmup_1 {
 
               return foo + bar;
         }
+
+       public boolean in3050(int a, int b) {
+            if (a >= 30 && a <= 40 && b >= 30 && b <= 40) {
+               return true;
+              }
+                  if (a >= 40 && a <= 50 && b >= 40 && b <= 50) {
+                  return true;
+              }
+              else
+              return false;
+        }
       
 
  
