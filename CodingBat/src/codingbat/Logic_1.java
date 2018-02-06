a291a580b1a3ab5d04f694cdfaab48a16a1c122c
Piotr Kazimierski <pi0tr3k9292@gmail.com>
2018-02-06 22:32:00 +0100

    specialEleven refactor

--- CodingBat/src/codingbat/Logic_1.java
+++ CodingBat/src/codingbat/Logic_1.java
@@ -45,11 +45,7 @@ public class Logic_1 {
     }
 
     public boolean specialEleven(int n) {
-
-      if(n%11 == 0 || n%11 == 1)
-      return true;
-      else
-      return false;
+     return (n%11 == 0 || n%11 == 1) ? true : false; 
     }
 
     public boolean cigarParty(int cigars, boolean isWeekend) {
