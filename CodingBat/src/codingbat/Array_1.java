21a3856b06326d9fdab00100e8ed8c50c4352d39
Piotr Kazimierski <pi0tr3k9292@gmail.com>
2019-05-06 21:16:06 +0200

    firstLast6 solution added

--- CodingBat/src/codingbat/Array_1.java
+++ CodingBat/src/codingbat/Array_1.java
@@ -1,1 +1,20 @@
+package codingbat;
 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author inquis1t0r
+ */
+
+public class Array_1 {
+  
+  public boolean firstLast6(int[] nums) {
+     return nums[0] == 6 || nums[nums.length - 1] == 6;
+  }
+  
+}
