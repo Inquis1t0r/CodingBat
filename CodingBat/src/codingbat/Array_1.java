d842b73c1b3a2e6ab00beebde99b5f76a81d95f7
Inquis1t0r <pi0tr3k9292@gmail.com>
2019-05-07 21:03:10 +0200

    sum2 attempt

--- CodingBat/src/codingbat/Array_1.java
+++ CodingBat/src/codingbat/Array_1.java
@@ -17,4 +17,26 @@ public class Array_1 {
      return nums[0] == 6 || nums[nums.length - 1] == 6;
   }
   
+  public int sum2(int[] nums) {
+    int result = 0;
+    
+    if(nums.length > 2)
+    {
+       result += nums[0];
+       result += nums[1];
+    }
+    else if (nums.length == 0)
+    {
+     result = 0;
+    }
+    else
+    {
+       for (int x : nums) {
+            result += nums[x];
+        }
+    }
+    return result;
+  }
+
+  
 }
