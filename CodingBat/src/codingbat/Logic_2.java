8df37553dba639c4b84374ed2ae4d3f52bfb91bb
Inquis1t0r <pi0tr3k9292@gmail.com>
2019-07-31 18:21:30 +0200

    Update Logic_2.java

--- CodingBat/src/codingbat/Logic_2.java
+++ CodingBat/src/codingbat/Logic_2.java
@@ -25,6 +25,32 @@ public class Logic_2 {
 
               return answer;
         }
+    
+            public boolean evenlySpaced(int a, int b, int c) {
+              //math.pow was not allowed :( 
+            int temp;
+
+            if(a > b) {
+                temp = a;
+                a = b;
+                b = temp;
+            }
+
+            if(b > c) {
+                temp = b;
+                b = c;
+                c = temp;
+            }
+
+            if(a > b) {
+                temp = a;
+                a = b;
+                b = temp;
+            }
+
+          return ((b - a) == (c - b));
+    }
+
 
     
 }
